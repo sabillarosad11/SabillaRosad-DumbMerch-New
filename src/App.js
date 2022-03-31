d42b6f20dde04e272e91e67cf54cbdf113b07d77
sabillarosad11 <sabillarosad114@gmail.com>
2022-03-31 11:21:56 +0700

    add login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import Login from './Pages/Login'
-import Register from "./Pages/Register";
+import Login from './Pages/Login'
+// import Register from "./Pages/Register";
 import './App.css';
 
 function App() {
   return (
-    <Register />
+    <Login />
   );
 }
 
